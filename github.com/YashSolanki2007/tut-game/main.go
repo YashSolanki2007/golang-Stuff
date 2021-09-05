5bbe36a3657dbf2f934b2d3dbaf24303d2fc4f77
YashSolanki2007 <70767472+YashSolanki2007@users.noreply.github.com>
2021-09-05 11:06:31 +0530

    Add files via upload

---
+++ github.com/YashSolanki2007/tut-game/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/YashSolanki2007/player.go"
+	// "github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	Get()
+	fmt.Println("df")
+}
---
+++ github.com/YashSolanki2007/tut-game/player.go
@@ -0,0 +1,7 @@
+package main
+
+import ("fmt")
+
+func Get() {
+	fmt.Println("got");
+}
